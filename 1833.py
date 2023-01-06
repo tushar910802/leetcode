7acd170d2f008677502dc0e2b7d774892e19ac1f
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2023-01-06 11:54:32 +0530

    Add files via upload

---
+++ 1833.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        ans = 0
+        for cost in costs:
+            if coins >= cost:
+                coins -= cost
+                ans += 1
+            else:
+                break
+        return ans
