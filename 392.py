3e6be429790e22ee600147572cfdb60308c3881a
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2023-09-22 18:29:58 +0530

    Add files via upload

--- 392.py
+++ 392.py
@@ -1,5 +1,17 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        """
+        Given two strings s and t, return whether or not s is a subsequence of t.
+
+        A subsequence of a string is a new string formed from the original string by deleting some (or no) of the characters without changing the relative order of the remaining characters. For example, "ace" is a subsequence of "abcde".
+
+        Args:
+            s: str
+            t: str
+
+        Returns:
+            bool
+        """
         if not s:
             return True
         
@@ -13,3 +25,6 @@ class Solution:
         if i == len(s):     
             return True
         return False
+
+
+## 392. Is Subsequence
