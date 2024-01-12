7bf6ecd74ffede5b57ea35826e78fa5f9d50b544
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2024-01-12 21:42:33 +0530

    Update 1704.py

--- 1704.py
+++ 1704.py
@@ -1,5 +1,29 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
+        """
+        Checks if the first and second halves of a given string have the same number
+        of vowels (case-insensitive).
+
+        Parameters:
+        - s (str): The input string to be checked.
+
+        Returns:
+        - bool: True if the number of vowels in the first half is equal to the number
+                of vowels in the second half, False otherwise.
+
+        Example:
+        halvesAreAlike("abcdeABCDE") should return True, as both halves "abcde" and "ABCDE"
+        contain the same number of vowels (2).
+
+        Note:
+        - Vowels are considered to be 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'.
+        - The input string can have even or odd length.
+
+        Complexity:
+        - Time: O(N), where N is the length of the input string.
+        - Space: O(1), as we use a constant amount of space for the vowel set and
+                variables to count the number of vowels.
+        """
         s1 =  "aeiouAEIOU"
         
         count_i1 = 0
