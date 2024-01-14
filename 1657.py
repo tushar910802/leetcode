6e5d5b93bc544f5030b83896261c197242676f7e
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2024-01-14 17:12:05 +0530

    Update 1657.py

--- 1657.py
+++ 1657.py
@@ -1,5 +1,32 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        """
+        Checks if two words are "close" under the following conditions:
+        1. They have the same length.
+        2. They have the same set of characters.
+
+        Two words are considered "close" if and only if the frequencies of all characters
+        in both words can be rearranged to form an anagram.
+
+        Parameters:
+        - word1 (str): The first input word.
+        - word2 (str): The second input word.
+
+        Returns:
+        - bool: True if the words are "close," False otherwise.
+
+        Example:
+        closeStrings("abc", "bca") should return True, as "abc" and "bca" are close words.
+
+        Note:
+        - The two words must have the same length to be considered close.
+        - Close words have the same set of characters, and the frequencies of these
+        characters can be rearranged to form an anagram.
+
+        Complexity:
+        - Time: O(N log N), where N is the length of the input words.
+        - Space: O(C), where C is the number of unique characters in the input words.
+        """
         if len(word1) != len(word2):
             return False
         
