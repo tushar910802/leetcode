ae44c69dc67914a5e2789274bd4d40f113a1e00a
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2024-01-18 17:34:58 +0530

    Update 70.py

--- 70.py
+++ 70.py
@@ -1,5 +1,29 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        """
+        Calculates the number of distinct ways to climb to the top of a staircase
+        with n steps, where you can climb either 1 or 2 steps at a time.
+
+        Parameters:
+        - n (int): The total number of steps in the staircase.
+
+        Returns:
+        - int: The number of distinct ways to climb to the top.
+
+        Example:
+        climbStairs(3) should return 3, as there are three ways to climb the
+        staircase: 1 + 1 + 1, 1 + 2, and 2 + 1.
+
+        Note:
+        - The function uses dynamic programming to calculate the number of ways.
+        - It builds an array 'f' where f[i] represents the number of ways to climb
+          to step i.
+        - The final result is stored in f[n-1].
+
+        Complexity:
+        - Time: O(N), where N is the number of steps in the staircase.
+        - Space: O(N), as the function uses an array of size N for dynamic programming.
+        """
         f = []
         
         f.append(1)
