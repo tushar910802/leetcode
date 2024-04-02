49a1ef5d84743bfe06127ef54629877f59a69992
Tushar Parekh <50999651+tushar910802@users.noreply.github.com>
2024-04-03 02:11:02 +0530

    Update 205.py

--- 205.py
+++ 205.py
@@ -1,5 +1,26 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        """
+        Determines if two strings are isomorphic, i.e., if characters in one string can be
+        replaced to get the other string.
+
+        Parameters:
+        - s (str): The first input string.
+        - t (str): The second input string.
+
+        Returns:
+        - bool: True if the strings are isomorphic, False otherwise.
+
+        Example:
+        isIsomorphic("egg", "add") should return True, as the characters 'e' and 'a' are
+        mapped to 'a', and 'g' and 'd' are mapped to 'd', resulting in isomorphic strings.
+
+        Note:
+        - The function uses two dictionaries to map characters from 's' to 't' and vice versa.
+        - It iterates through the strings and checks if the mappings are consistent.
+        - If the mappings are not consistent, the function returns False.
+        - The result is True if the strings are isomorphic, False otherwise.
+        """
         letter_map = {}
         reverse_map = {}
 
